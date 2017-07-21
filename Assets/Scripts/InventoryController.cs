10ddfaa07cbce6a5cc5d218fd816e996b2135a41
Jonathan Lee <jsalinlee@gmail.com>
2017-07-21 12:04:38 -0700

    Loading items from database into inventory

--- Assets/Scripts/InventoryController.cs
+++ Assets/Scripts/InventoryController.cs
@@ -1,16 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryController : MonoBehaviour {
+	GameObject inventoryPanel;
+	GameObject slotPanel;
+	ItemDatabase database;
+	public GameObject inventorySlot;
+	public GameObject inventoryItem;
 
-	// Use this for initialization
-	void Start () {
-		
+	private int slotCount;
+
+	public List<Item> items = new List<Item>();
+	public List<GameObject> slots = new List<GameObject>();
+
+	void Start() {
+		database = GetComponent<ItemDatabase>();
+		slotCount = 20;
+		inventoryPanel = GameObject.Find("Inventory Panel");
+		slotPanel = inventoryPanel.transform.Find("Slot Panel").gameObject;
+
+		for(int i = 0; i < slotCount; i++) {
+			items.Add(new Item());
+			slots.Add(Instantiate(inventorySlot));
+			slots[i].transform.SetParent(slotPanel.transform);
+		}
+		AddItem(0);
+		AddItem(1);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void AddItem(int id) {
+		Item itemToAdd = database.FetchItemByID(id);
+		for(int i = 0; i < items.Count; i++) {
+			if(items[i].ID == -1) {
+				items[i] = itemToAdd;
+				GameObject itemObj = Instantiate(inventoryItem);
+				itemObj.transform.SetParent(slots[i].transform);
+				itemObj.transform.position = Vector2.zero;
+				itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+				itemObj.name = itemToAdd.Title;
+				slots[i].name = itemToAdd.Title + " Slot";
+				break;
+			}
+		}
 	}
 }
--- Assets/Scripts/ItemDatabase.cs
+++ Assets/Scripts/ItemDatabase.cs
@@ -14,12 +14,24 @@ public class ItemDatabase : MonoBehaviour {
 //		Debug.Log(database[0].Title);
 		itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
 		ConstructItemDatabase();
-		Debug.Log(database[0].Title);
+		Debug.Log(FetchItemByID(0).Description);
+	}
+
+	public Item FetchItemByID(int id) {
+		for(int i = 0; i < database.Count; i++) {
+			if(database[i].ID == id) {
+				return database[i];
+			}
+		}
+		return null;
 	}
 
 	void ConstructItemDatabase() {
 		for (int i = 0; i < itemData.Count; i++) {
-			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"]));
+			database.Add(new Item((int)itemData[i]["id"], itemData[i]["title"].ToString(), (int)itemData[i]["value"],
+				(int)itemData[i]["stats"]["power"], (int)itemData[i]["stats"]["defense"], (int)itemData[i]["stats"]["vitality"],
+				itemData[i]["description"].ToString(), (bool)itemData[i]["stackable"], (int)itemData[i]["rarity"], itemData[i]["slug"].ToString()
+			));
 		}
 	}
 }
@@ -35,10 +47,23 @@ public class Item {
 	public bool Stackable { get; set; }
 	public int Rarity { get; set; }
 	public string Slug { get; set; }
+	public Sprite Sprite { get; set; }
 
-	public Item(int id, string title, int value) {
+	public Item(int id, string title, int value, int power, int defense, int vitality, string description, bool stackable, int rarity, string slug) {
 		this.ID = id;
 		this.Title = title;
 		this.Value = value;
+		this.Power = power;
+		this.Defense = defense;
+		this.Vitality = vitality;
+		this.Description = description;
+		this.Stackable = stackable;
+		this.Rarity = rarity;
+		this.Slug = slug;
+		this.Sprite = Resources.Load<Sprite>("Sprites/Items/" + slug);
+	}
+
+	public Item() {
+		this.ID = -1;
 	}
 }
