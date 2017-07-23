135481b05d7dbe3b658b79e0f08a24c94492d1ec
rgowen <ryan.gowen@gmail.com>
2017-07-23 08:06:44 -0700

    Add SetTarget and SetCameraSize

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,16 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-
+    public Camera mainCamera;
     public GameObject followTarget;
     private Vector3 targetPostion;
     public float moveSpeed;
-	
+    public int cameraSize;
 	void Start () {
-		
+        mainCamera = GetComponent<Camera>();
+        SetCameraSize(cameraSize);
 	}
-	
-	
+    public void SetTarget(GameObject newTarget)
+    {
+        followTarget = newTarget;
+    }
+
+    public void SetCameraSize(int size)
+    {
+        mainCamera.orthographicSize = size;
+    }
 	void Update ()
     {
         targetPostion = new Vector3(followTarget.transform.position.x, followTarget.transform.position.y, transform.position.z);
