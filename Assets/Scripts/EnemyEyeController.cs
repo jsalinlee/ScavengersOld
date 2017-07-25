8366a47aba6254a170dfd602f971e7a26cf6c44a
DEXTER-PC-MKII\Dexter <nicimoc@gmail.com>
2017-07-24 19:07:17 -0700

    Enemy
    Attempted to make an enemy eyebat thing that moved periodically. Unity thinks there is no rigidbody attached to the eyebat thing when there clearly is.

---
+++ Assets/Scripts/EnemyEyeController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyEyeController : MonoBehaviour
+{
+    public float moveSpeed;
+    public Rigidbody myRigidbody;
+    private bool moving;
+    public float timeBetweenMove;
+    private float timebetweenMoveCounter;
+    public float timeToMove;
+    private float timeToMoveCounter;
+    private Vector3 movedirection;
+
+
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody>();
+        timebetweenMoveCounter = timeBetweenMove;
+        timeToMoveCounter = timeToMove;
+    }
+
+    void Update()
+    {
+        if (moving)
+        {
+            timeToMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = movedirection;
+
+            if (timeToMoveCounter < 0f)
+            {
+                moving = false;
+                timebetweenMoveCounter = timeBetweenMove;
+            }
+        }
+        else
+        {
+            timebetweenMoveCounter -= Time.deltaTime;
+            myRigidbody.velocity = Vector2.zero;
+            if (timebetweenMoveCounter < 0f)
+            moving = true;
+            timeToMoveCounter = timeToMove;
+
+            movedirection = new Vector3(Random.Range(-1f, 1f) * moveSpeed, Random.Range(-1f, 1f) * moveSpeed, 0f);
+        }
+    }
+
+
+}
